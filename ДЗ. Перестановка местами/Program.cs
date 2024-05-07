e68c135b6cfef12af7278349741c0a04b0ad7783
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-05-08 00:47:00 +0500

    Добавьте файлы проекта.

---
+++ ДЗ. Перестановка местами/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ДЗ.Перестановка_местами
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "London";
+            string surname = "Jack";
+            Console.WriteLine($"Ваше имя {name}, а фамилия {surname} ?");
+            string correctSurname;
+            string correctName;
+            correctName =surname;
+            correctSurname = name;
+            Console.WriteLine($"Нет, моё имя {correctName}, а фамилия {correctSurname}");
+
+            string largeCup = "кофе";
+            string smaillСup = "чай";
+            Console.WriteLine($"Вам нужно в большой чашке {largeCup}, а в маленькой {smaillСup} ?");
+            string rideDringInLargeCup=smaillСup;
+            string rideDringInSmallCup=largeCup;
+            Console.WriteLine($"Принесите в большой чашке {rideDringInLargeCup}, а в маленькой {rideDringInSmallCup}");
+
+        }
+    }
+}

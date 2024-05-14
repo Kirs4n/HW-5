6ac94c1f576487d62b80246d16f4193862625838
Kirs4n <165835810+Kirs4n@users.noreply.github.com>
2024-05-15 00:16:46 +0500

    Add files via upload

--- ДЗ. Перестановка местами/Program.cs
+++ ДЗ. Перестановка местами/Program.cs
@@ -9,18 +9,14 @@ namespace ДЗ.Перестановка_местами
             string name = "London";
             string surname = "Jack";
             Console.WriteLine($"Ваше имя {name}, а фамилия {surname} ?");
-            string correctSurname;
-            string correctName;
-            correctName =surname;
-            correctSurname = name;
-            Console.WriteLine($"Нет, моё имя {correctName}, а фамилия {correctSurname}");
+            (name,surname)=(surname,name);
+            Console.WriteLine($"Нет, моё имя {name}, а фамилия {surname}");
 
             string largeCup = "кофе";
             string smaillСup = "чай";
             Console.WriteLine($"Вам нужно в большой чашке {largeCup}, а в маленькой {smaillСup} ?");
-            string rideDringInLargeCup=smaillСup;
-            string rideDringInSmallCup=largeCup;
-            Console.WriteLine($"Принесите в большой чашке {rideDringInLargeCup}, а в маленькой {rideDringInSmallCup}");
+            (largeCup,smaillСup) = (smaillСup, largeCup);
+            Console.WriteLine($"Принесите в большой чашке {largeCup} , а в маленькой  {smaillСup}");
 
         }
     }
